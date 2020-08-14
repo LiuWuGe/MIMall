4ddb0bf577e82d268c63f96d22d83bafbf66ad4c
LiuWuGe <971859466@qq.com>
2020-08-14 14:25:00 +0800

    目录结构

---
+++ src/pages/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <NavHeader><NavHeader/>
+  <router-view></router-view>
+  <NavFooter><NavFooter/>
+</template>
+
+<script>
+import NavHeader from '../components/NavHeader'
+import NavFooter from '../components/NavFooter'
+export default {
+  name: 'home',
+  components: {
+    NavHeader,
+    NavFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
